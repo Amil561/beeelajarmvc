b0d349672c292295b6617d68c88cff0ed4c8ef9d
Amil561 <amilmaaruf@gmail.com>
2024-03-20 09:47:34 +0700

    menambahkan htaccess dan routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,8 +1,27 @@
+
 <?php
 class App{
     public function __construct()
     {
-        echo 'berhasil masuk ke kelas utama app';
+        $url = $this->perseURL();
+        var_dump($url);
+    }
+
+    public function perseURL()
+    {
+        if (isset($_GET['url'])) {
+            //->Ambil value urlnya.
+            $url = $_GET['url'];
+            //->buang tanda '/'di akhir url.
+            $url = rtrim($url, '/');
+            //->filter url dari karakter2 aneh.
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //->pecah url berdasarkan tanda '/' sebagai pemisah
+            $url = explode('/', $url);
+
+            return $url;
+
+        }
     }
 } 
 ?>
